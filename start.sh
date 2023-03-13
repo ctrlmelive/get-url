705cd82f71e5297f179dd5bfa512b144f926c643
ctrlmelive <118881107+ctrlmelive@users.noreply.github.com>
2023-03-13 00:56:20 -0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 10
+while [ "a" = "a" ]
+do
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+  curl https://nosy-lily-bamboo.glitch.me/
+  curl https://oval-humane-pigment.glitch.me/
+  curl https://aerial-quasar-study.glitch.me/
+  sleep 7m
+done
